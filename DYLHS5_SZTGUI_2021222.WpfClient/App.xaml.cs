1d3171d553b472eb4e51287a13b47584d21439cd
Blitz428 <szegedi.tamas@stud.uni-obuda.hu>
2022-04-26 13:42:37 +0200

    WpfClient project and RestCollection class by Kovács András has been added.

---
+++ DYLHS5_SZTGUI_2021222.WpfClient/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DYLHS5_SZTGUI_2021222.WpfClient
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ DYLHS5_SZTGUI_2021222.WpfClient/MainWindowViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DYLHS5_SZTGUI_2021222.WpfClient
+{
+    class MainWindowViewModel
+    {
+    }
+}
