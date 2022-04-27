7680998b1e1b2f41098a9cf1f06b99d918467b38
hallgato <hallgato@LABOR113-16>
2022-04-27 11:58:57 +0200

    CRUD methods are written and working in every tables.

--- DYLHS5_SZTGUI_2021222.WpfClient/ViewModels/OrderWindowViewModel.cs
+++ DYLHS5_SZTGUI_2021222.WpfClient/ViewModels/OrderWindowViewModel.cs
@@ -1,24 +1,73 @@
 ﻿using DYLHS5_HFT_2021221.Models;
-using System;
-using System.Collections.Generic;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Input;
 
 namespace DYLHS5_SZTGUI_2021222.WpfClient
 {
-    class OrderWindowViewModel
+    class OrderWindowViewModel : ObservableRecipient
     {
         public RestCollection<Order> Orders { get; set; }
+        public RestCollection<Customer> Customers { get; set; }
+        public RestCollection<Product> Products { get; set; }
+
+        private Order selectedOrder;
+
+        public Order SelectedOrder
+        {
+            get { return selectedOrder; }
+            set
+            {
+                if (value != null)
+                {
+                    selectedOrder = new Order()
+                    {
+                        OrderId = value.OrderId,
+                        OrderTime = value.OrderTime,
+                        Customer = value.Customer,
+                        CustomerId = value.CustomerId,
+                        IsPrePaid = value.IsPrePaid,
+                        IsTransportRequired = value.IsTransportRequired,
+                        ProductId = value.ProductId,
+                        Product = value.Product
+
+                    };
+                    OnPropertyChanged();
+                    (DeleteOrderCommand as RelayCommand).NotifyCanExecuteChanged();
+                }
+            }
+        }
+
+        public ICommand UpdateOrderCommand { get; set; }
+        public ICommand CreateOrderCommand { get; set; }
+        public ICommand DeleteOrderCommand { get; set; }
+
+
+
         public OrderWindowViewModel()
         {
             if (!IsInDesignMode)
             {
                 Orders = new RestCollection<Order>("http://localhost:27588/", "order");
+                Products = new RestCollection<Product>("http://localhost:27588/", "product");
+                Customers = new RestCollection<Customer>("http://localhost:27588/", "customer");
+
+                CreateOrderCommand = new RelayCommand(() =>
+                {
+                    Orders.Add(new Order() { OrderId = SelectedOrder.OrderId, Customer = SelectedOrder.Customer, Product = SelectedOrder.Product, ProductId = SelectedOrder.ProductId, CustomerId = SelectedOrder.CustomerId, IsPrePaid = SelectedOrder.IsPrePaid, IsTransportRequired = SelectedOrder.IsTransportRequired, OrderTime = SelectedOrder.OrderTime });
+                });
+                DeleteOrderCommand = new RelayCommand(() =>
+                {
+                    Orders.Delete((int)SelectedOrder.OrderId);
+                }, () => { return SelectedOrder != null; });
+                UpdateOrderCommand = new RelayCommand(() =>
+                {
+                    Orders.Update(SelectedOrder);
+                }, () => { return SelectedOrder != null; });
             }
-            
+            SelectedOrder = new Order();
         }
         public static bool IsInDesignMode
         {
@@ -30,4 +79,7 @@ namespace DYLHS5_SZTGUI_2021222.WpfClient
 
         }
     }
+
+
 }
+
--- DYLHS5_SZTGUI_2021222.WpfClient/ViewModels/ProductWindowViewModel.cs
+++ DYLHS5_SZTGUI_2021222.WpfClient/ViewModels/ProductWindowViewModel.cs
@@ -1,26 +1,69 @@
 ﻿using DYLHS5_HFT_2021221.Models;
-using System;
-using System.Collections.Generic;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Input;
 
 namespace DYLHS5_SZTGUI_2021222.WpfClient
 {
-    class ProductWindowViewModel
+    class ProductWindowViewModel : ObservableRecipient
     {
         public RestCollection<Product> Products { get; set; }
+
+
+        private Product selectedProduct;
+
+        public Product SelectedProduct
+        {
+            get { return selectedProduct; }
+            set
+            {
+                if (value != null)
+                {
+                    selectedProduct = new Product()
+                    {
+                        ProductId = value.ProductId,
+                        Color = value.Color,
+                        Orders = value.Orders,
+                        Price = value.Price,
+                        ProductName = value.ProductName,
+                        Size = value.Size
+
+
+                    };
+                    OnPropertyChanged();
+                    (DeleteProductCommand as RelayCommand).NotifyCanExecuteChanged();
+                }
+            }
+        }
+
+        public ICommand UpdateProductCommand { get; set; }
+        public ICommand CreateProductCommand { get; set; }
+        public ICommand DeleteProductCommand { get; set; }
+
+
+
         public ProductWindowViewModel()
         {
             if (!IsInDesignMode)
             {
-                Products = new RestCollection<Product>("http://localhost:27588/", "product");
+                Products = new RestCollection<Product>("http://localhost:27588/", "Product");
+                CreateProductCommand = new RelayCommand(() =>
+                {
+                    Products.Add(new Product() { Size = SelectedProduct.Size, ProductName = SelectedProduct.ProductName, Price = SelectedProduct.Price, Color = SelectedProduct.Color, Orders = SelectedProduct.Orders, ProductId = SelectedProduct.ProductId });
+                });
+                DeleteProductCommand = new RelayCommand(() =>
+                {
+                    Products.Delete((int)SelectedProduct.ProductId);
+                }, () => { return SelectedProduct != null; });
+                UpdateProductCommand = new RelayCommand(() =>
+                {
+                    Products.Update(SelectedProduct);
+                }, () => { return SelectedProduct != null; });
             }
-            
+            SelectedProduct = new Product();
         }
-
         public static bool IsInDesignMode
         {
             get
@@ -32,3 +75,4 @@ namespace DYLHS5_SZTGUI_2021222.WpfClient
         }
     }
 }
+
