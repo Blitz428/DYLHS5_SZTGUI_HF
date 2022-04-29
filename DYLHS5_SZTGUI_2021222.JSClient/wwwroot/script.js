537b5c9779948f35a75790179600579ecf743740
Blitz428 <szegedi.tamas@stud.uni-obuda.hu>
2022-04-29 20:39:53 +0200

    SignalR and Update method working.

--- DYLHS5_SZTGUI_2021222.JSClient/wwwroot/script.js
+++ DYLHS5_SZTGUI_2021222.JSClient/wwwroot/script.js
@@ -1,13 +1,52 @@
 ﻿let products = [];
+let connection = null;
 
+let productIdToUpdate = -1;
 getdata();
+setupSignalR();
+
+function setupSignalR() {
+    connection = new signalR.HubConnectionBuilder()
+        .withUrl("http://localhost:27588/hub")
+        .configureLogging(signalR.LogLevel.Information)
+        .build();
+
+    connection.on("ProductCreated", (user, message) => {
+        getdata();
+    });
+    connection.on("ProductDeleted", (user, message) => {
+        getdata();
+    });
+    connection.on("ProductUpdated", (user, message) => {
+        getdata();
+    });
+
+    connection.onclose
+        (async () => {
+            await start();
+        });
+    start();
+
+
+}
+
+
+async function start() {
+    try {
+        await connection.start();
+        console.log("SignalR Connected.");
+    } catch (err) {
+        console.log(err);
+        setTimeout(start, 5000);
+    }
+};
 
 async function getdata() {
     await fetch('http://localhost:27588/product')
         .then(x => x.json())
         .then(y => {
             products = y;
-            console.log(products);
+            //console.log(products);
             display();
         });
 }
@@ -25,7 +64,8 @@ function display() {
             + t.color + "</td><td>"
             + t.size + "</td><td>"
             + t.price + "</td><td>"
-            + `<button type="button" onclick="remove(${t.productId})">Delete</button>` + "</td></tr>"
+        + `<button type="button" onclick="remove(${t.productId})">Delete</button>`
+        + `<button type="button" onclick="showupdate(${t.productId})">Update</button>`  + "</td></tr>"
     });
 }
 
@@ -54,6 +94,31 @@ function remove(productId){
         .then(response => response)
         .then(data => { console.log('Success:', data); getdata(); })
         .catch(error => { console.error('Error:', error); });
+}
 
+function showupdate(productId) {
+    document.getElementById('productNameToUpdate').value = products.find(t => t['productId'] == productId)['productName']
+    document.getElementById('colorToUpdate').value = products.find(t => t['productId'] == productId)['color']
+    document.getElementById('sizeToUpdate').value = products.find(t => t['productId'] == productId)['size']
+    document.getElementById('priceToUpdate').value = products.find(t => t['productId'] == productId)['price']
+    document.getElementById('updateformdiv').style.display = 'flex';
+    productIdToUpdate = productId;
+}
 
+function update() {
+    let id = productIdToUpdate;
+    let name = document.getElementById('productNameToUpdate').value;
+    let color = document.getElementById('colorToUpdate').value;
+    let size = document.getElementById('sizeToUpdate').value;
+    let price = document.getElementById('priceToUpdate').value;
+    fetch('http://localhost:27588/product/', {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json', },
+        body: JSON.stringify(
+            { productId: id, productName: name, color: color, size: size, price: price }),
+    })
+        .then(response => response)
+        .then(data => { console.log('Success:', data); getdata(); })
+        .catch(error => { console.error('Error:', error); });
+    document.getElementById('updateformdiv').style.display = 'none';
 }
